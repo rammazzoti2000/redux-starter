ef512fe323b91e729836d0f5513604cfcd9e48ca
Alexandru <rammazzoti2000@gmail.com>
2021-04-09 13:55:50 +0200

    Create reducer

---
+++ src/reducer.js
@@ -0,0 +1,21 @@
+// []
+let lastId = 0;
+
+function reducer(state = [], action) {
+    switch (action.type) {
+        case 'bugAdded':
+            return [
+                ...state,
+                {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false,
+                }
+            ];
+        case 'bugRemoved':
+            return state.filter(bug => Buffer.id !== action.payload.id);
+        
+        default:
+            return state;
+    }
+}

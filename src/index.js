fe18f14b9ab9935cd7d465c072083023697a3d2f
Alexandru <rammazzoti2000@gmail.com>
2021-04-14 16:18:52 +0200

    Build Redux from scratch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import store from './store';
-import { bugAdded, bugResolved } from './actions';
+import store from './customStore';
+import * as actions from './actions';
 
-// const unsubscribe = store.subscribe(() => {
-//     console.log('Store changed!', store.getState());
-// });
+store.subscribe(() => {
+    console.log('store changed')
+}) 
 
-store.dispatch(bugAdded('Bug 1'));
-
-// unsubscribe();
-
-store.dispatch(bugResolved(1));
-
-console.log(store.getState());
+store.dispatch(actions.bugAdded('Bug 1'))
+console.log(store.getState())

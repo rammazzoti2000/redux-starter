b86fea58b4222946b798dc4e09a3f8741970c84b
Alexandru <rammazzoti2000@gmail.com>
2021-04-14 17:04:57 +0200

    DevToolsEnhancer to trace actions

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ store.subscribe(() => {
     console.log('store changed')
 }) 
 
-store.dispatch(actions.bugAdded('Bug 1'))
+store.dispatch(actions.bugAdded('Bug 1'));
+store.dispatch(actions.bugAdded('Bug 2'));
+store.dispatch(actions.bugAdded('Bug 3'));
+store.dispatch(actions.bugResolved(1));
 console.log(store.getState())
